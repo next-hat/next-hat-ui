2035733be8dd44917a87f520f811917b70901ed6
leon3s <leo.vernisse@gmail.com>
2025-12-18 16:08:47 +0100

    updated import

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@next-hat/ui/components/ui/toast"
+import { useToast } from "@next-hat/ui/components/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
