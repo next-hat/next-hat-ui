7d81a7f899f99f6a1d9245b91300209fffca69e7
leone <leone@next-hat.com>
2026-09-13 15:03:16 +0200

    chore: update shadcn

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,14 +1,9 @@
-import { cn } from "@next-hat/ui/lib/utils"
-import { FiLoader } from "react-icons/fi"
+import { cn } from "cn"
+import { Loader2Icon } from "lucide-react"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <FiLoader
-      role="status"
-      aria-label="Loading"
-      className={cn("size-4 animate-spin", className)}
-      {...props}
-    />
+    <Loader2Icon data-slot="spinner" role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
 }
 
--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cn } from "@next-hat/ui/lib/utils"
-import * as ToastPrimitives from "@radix-ui/react-toast"
+import { Toast as ToastPrimitives } from "radix-ui"
 import { cva, type VariantProps } from "class-variance-authority"
 import { FiX } from "react-icons/fi"
 
